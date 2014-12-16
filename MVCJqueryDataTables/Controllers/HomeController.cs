e9869da71269bb3eeb53d01ba3ec4fe5d8732c2a
gngrbrd <friedman.richard@gmail.com>
2014-12-16 00:48:31 -0500

    MVCJqueryDataTables initial commit

---
+++ MVCJqueryDataTables/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCJqueryDataTables.Models;
+
+namespace MVCJqueryDataTables.Controllers
+{
+    public class HomeController : Controller
+    {
+        private CustomerListDataContext _dataModel;
+        public HomeController()
+        {
+            _dataModel = new CustomerListDataContext();
+        }
+
+        public ActionResult Index()
+        {
+            return View(_dataModel.Customers.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
